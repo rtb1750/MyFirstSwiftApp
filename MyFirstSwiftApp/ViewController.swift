3d8baadb1682c9b17c55ddddce1d35490dabbd4d
Ryan <rtb1750@icloud.com>
2019-07-17 12:40:52 -0700

    MyFirstSwiftApp Edited

--- MyFirstSwiftApp/ViewController.swift
+++ MyFirstSwiftApp/ViewController.swift
@@ -14,7 +14,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBOutlet weak var ImageView1: UIImageView!
+    var showFirstImage:Bool = false
 
 
+    
+    
+    @IBAction func button(_ sender: Any) {
+        if (showFirstImage == false){
+             ImageView1.image = UIImage(named: "Pikachu")
+            showFirstImage = true
+        } else {
+            ImageView1.image = UIImage(named: "Psyduck")
+            showFirstImage = false
+    
+        }
 }
 
+}
